eb49e05f62dc640cea936de980cdd6270910835f
yuya uchida <u.yuya0616ll@gmail.com>
2021-07-30 14:42:03 +0900

    Add Login Function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = t('notice.login_needed')
+      redirect_to new_session_path
+    end
+  end
 end
