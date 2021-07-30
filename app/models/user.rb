c9bcbb5339d16290cda3add4e3c7468b31262389
yuya uchida <u.yuya0616ll@gmail.com>
2021-07-30 14:16:34 +0900

    Add Session Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  mount_uploader :icon, IconUploader
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new]
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :articles do
     collection do
       post :confirm
